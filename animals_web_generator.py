672d5776c0109fb9bee4df866c269c6cd6813eea
m <m@m.com>
2024-11-23 15:49:20 +0100

    added original code from zootopia_github and added the api_key

---
+++ animals_web_generator.py
@@ -0,0 +1,61 @@
+import json
+import requests
+
+API_KEY = "rxM8D6n6RrdmmJA5uW2KQg==pJcpzaY9CtCVGhDg"
+API_URL = "https://api.api-ninjas.com/v1/animals?name={}"
+
+#new function to get data from api based on user imput
+def fetch_data_from_api(animal_name):
+
+
+#to be deleted
+def load_data(file_path):
+    with open(file_path, "r") as handle:
+        return json.load(handle)
+
+def serialize_animal(animal_obj):
+    output = '<li class="cards__item">\n'
+    output += f'  <div class="card__title">{animal_obj["name"]}</div>\n'
+    output += '  <p class="card__text">\n'
+    
+    if 'characteristics' in animal_obj:
+        if 'diet' in animal_obj['characteristics']:
+            output += f'      <strong>Diet:</strong> {animal_obj["characteristics"]["diet"]}<br/>\n'
+        if 'locations' in animal_obj and animal_obj['locations']:
+            output += f'      <strong>Location:</strong> {", ".join(animal_obj["locations"])}<br/>\n'
+        if 'type' in animal_obj['characteristics']:
+            output += f'      <strong>Type:</strong> {animal_obj["characteristics"]["type"]}<br/>\n'
+    output += '  </p>\n'
+    output += '</li>\n'
+    return output
+
+def generate_animal_cards(data):
+    output = ''
+    for animal_obj in data:
+        output += serialize_animal(animal_obj)
+    return output
+
+def update_html_template(template_path, output_path, animals_info):
+    with open(template_path, "r") as file:
+        html_content = file.read()
+    updated_content = html_content.replace("__REPLACE_ANIMALS_INFO__", animals_info)
+    with open(output_path, "w") as file:
+        file.write(updated_content)
+
+#add user input
+def main():
+    
+    animal_name = input("Search an animal: ")
+    
+    json_file_path = "animals_data.json"
+    template_file_path = "animals_template.html"
+    output_file_path = "animals.html"
+    
+    animals_data = load_data(json_file_path)
+    
+    animals_info = generate_animal_cards(animals_data)
+    
+    update_html_template(template_file_path, output_file_path, animals_info)
+
+if __name__ == "__main__":
+    main()
