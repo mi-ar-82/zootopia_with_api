9fcfb03e043cb937797b78695cae7972ac9c5d89
m <m@m.com>
2024-11-23 17:27:17 +0100

    milestone 3 : added error handling

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -4,7 +4,7 @@ import requests
 API_KEY = "rxM8D6n6RrdmmJA5uW2KQg==pJcpzaY9CtCVGhDg"
 API_URL = "https://api.api-ninjas.com/v1/animals?name={}"
 
-#new function to get data from api based on user imput
+
 def fetch_data_from_api(animal_name):
     url = API_URL.format(animal_name)
     headers = {'X-Api-Key': API_KEY}
@@ -13,12 +13,12 @@ def fetch_data_from_api(animal_name):
     if response.status_code == 200:
         return response.json()
     else:
-        print("Error:", response.status_code, response.text)
+        raise Exception(f"Failed to fetch data: {response.status_code}, {response.text}")
 
-#to be deleted
-# def load_data(file_path):
-#     with open(file_path, "r") as handle:
-#         return json.load(handle)
+
+
+
+
 
 def serialize_animal(animal_obj):
     output = '<li class="cards__item">\n'
@@ -49,20 +49,28 @@ def update_html_template(template_path, output_path, animals_info):
     with open(output_path, "w") as file:
         file.write(updated_content)
 
-#add user input
+
 def main():
-    
     animal_name = input("Search an animal: ")
     
-    json_file_path = "animals_data.json"
     template_file_path = "animals_template.html"
     output_file_path = "animals.html"
+    try:
+        animals_data = fetch_data_from_api(animal_name)
+        if not animals_data:
+            error_message = f'<h2>The animal "{animal_name}" does not exist.</h2>'
+            update_html_template(template_file_path, output_file_path, error_message)
+            print(f"No data found for '{animal_name}'. A message has been added to '{output_file_path}'.")
+            return
+        animals_info = generate_animal_cards(animals_data)
+        update_html_template(template_file_path, output_file_path, animals_info)
+        print(f"HTML file generated successfully! Open '{output_file_path}' to view it.")
     
-    animals_data = fetch_data_from_api(animal_name)  #changed function
+    except Exception as e:
+        error_message = f'<h2>An error occurred: {e}</h2>'
+        update_html_template(template_file_path, output_file_path, error_message)
+        print(f"Error: {e}. A message has been added to '{output_file_path}'.")
     
-    animals_info = generate_animal_cards(animals_data)
     
-    update_html_template(template_file_path, output_file_path, animals_info)
-
 if __name__ == "__main__":
     main()
